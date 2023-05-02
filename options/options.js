7affa930be0c63c759a5d0c749dbf2a6829b9292
Crowdbotics <engineering@crowdbotics.com>
2023-05-02 11:35:44 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "proud-river-dev-80232",
+  url: "https://proud-river-dev-80232.botics.co",
+  api: "https://proud-river-dev-80232.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
